1e4bada6cea6a0f9cb09fe285b85f1773b4923c9
BaxtiyorBozorov <baxtiyorbozorov226@gmail.com>
2025-04-01 01:06:09 +0500

    application module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { SshModule1 } from './modules/v1/auto-install/ssh.module';
 import { ProductModule } from './modules/v1/product/product.module';
 import { SshModule } from './modules/v1/ssh-connection/ssh.connection.module';
 import { UserModule } from './modules/v1/user/user.module';
-import { ComputerModule } from './modules/v1/computer/comp.module';
+import { ComputerModule } from './modules/v1/application/comp.module';
 import { WebSocketModule } from './modules/v1/websocket/ws.module';
 
 @Module({
---
+++ src/modules/v1/application/comp.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common';
+import db from 'src/config/database.config';
+import { AppInterface } from '../agent/entity/app.interface';
+import { ComputerInterface } from '../agent/entity/computer.interface';
+import { Socket } from 'socket.io';
+
+@Injectable()
+export class ComputerService {
+    async getAllComputers(
+        page: number,
+        limit: number,
+        agentConnections: Map<string, Socket>
+    ): Promise<{ computers: ComputerInterface[]; total: number; page: number }> {
+        const total = await db('computers').count('id as total');
+        const offset = (page - 1) * limit;
+
+        const computers = await db('computers').select('*').limit(limit).offset(offset);
+
+        const computersWithStatus = computers.map((computer) => { 
+            const isActive = agentConnections.has(computer.id)
+            return {
+                ...computer,
+                status: isActive ? 'active' : 'inactive', // Statusni qo'shish
+            };
+        })
+
+        computersWithStatus.sort((a, b) => {
+            if (a.status === 'active' && b.status === 'inactive') return -1;
+            if (a.status === 'inactive' && b.status === 'active') return 1;
+            return 0;
+        });
+
+        return {
+            computers: computersWithStatus,
+            total: Number(total[0].total),
+            page,
+        };
+    }
+    async getComputerById(id: string): Promise<ComputerInterface | null> {
+        const computer = await db('computers').where('id', id).first();
+        return computer;
+    }
+
+    async getApplicationByComputerId(
+        computerId: string,
+        page: number,
+        limit: number,
+    ): Promise<{ applications: AppInterface[]; total: number; page: number }> {
+        const offset = (page - 1) * limit;
+        const total = await db('apps').where('computer_id', computerId).count('id as total');
+        const applications = await db('apps')
+            .where('computer_id', computerId)
+            .select('*')
+            .limit(limit)
+            .offset(offset);
+
+        return {
+            applications,
+            total: Number(total[0].total),
+            page,
+        };
+    }
+}
