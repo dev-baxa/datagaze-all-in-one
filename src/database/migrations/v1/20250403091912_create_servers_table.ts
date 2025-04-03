8355a4176cdcb708a8c839e01f224557dbd42311
BaxtiyorBozorov <baxtiyorbozorov226@gmail.com>
2025-04-03 14:40:06 +0500

    update all migrations files

--- src/database/migrations/v1/20250403091912_create_servers_table.ts
+++ src/database/migrations/v1/20250403091912_create_servers_table.ts
@@ -1,14 +1,14 @@
-import { Knex } from 'knex';
+import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('servers', table => {
         table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
-        table.string('name').notNullable();
+        // Removed 'name' in 20250330181440_update_server
         table.string('ip_address').notNullable();
         table.integer('port').notNullable();
-        table.string('status').defaultTo('active');
-        table.uuid('user_id').references('id').inTable('users').onDelete('CASCADE');
-        table.jsonb('specifications').nullable();
+        table.enum('os_type', ['linux', 'windows', 'macOs']).defaultTo('linux'); // From 20250219125446_update_servers
+        table.string('username').nullable(); // From 20250219125446_update_servers
+        // password and private_key were dropped in 20250402040051_update_servers
         table.timestamps(true, true);
     });
 }
